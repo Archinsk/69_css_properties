10d0368eede3731ce39be045403badb0fb4ba683
Mikhail Vasilev <mgp-nsk@yandex.ru>
2023-04-04 17:39:53 +0700

    border-style types

--- src/views/BordersView.vue
+++ src/views/BordersView.vue
@@ -1,68 +1,60 @@
 <template>
-  <div>
+  <div id="borders">
     <h3>Borders</h3>
-    <div class="buttons">
-      <button>default</button>
-      <button class="btn-transparent">Button</button>
-      <button class="btn-transparent btn-bordered">Button</button>
-      <button class="btn-transparent btn-only-left-border">Button</button>
-      <button class="btn-transparent btn-only-left-border-facet">Button</button>
-      <button class="btn-transparent btn-bordered rounded">Button</button>
-      <button class="btn-transparent btn-only-left-border rounded">
-        Button
-      </button>
-      <button class="btn-transparent btn-only-left-border-facet rounded">
-        Button
-      </button>
-      <h4>
-        border-style (border-top-style, border-right-style, border-bottom-style,
-        border-left-style)
-      </h4>
-      <div class="example-group">
-        <div class="display-flex align-items-center">
-          <div class="example-brick"></div>
-          <div>default (without border-style)</div>
+    <h4>
+      1. border-style (border-top-style, border-right-style,
+      border-bottom-style, border-left-style)
+    </h4>
+    <div class="example-group">
+      <div class="display-flex align-items-center">
+        <div class="example-brick"></div>
+        <div>default (without border-style)</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-none"></div>
+        <div>border-style: none;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-hidden"></div>
+        <div>border-style: hidden;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-dotted"></div>
+        <div>border-style: dotted;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-dashed"></div>
+        <div>border-style: dashed;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-solid"></div>
+        <div>border-style: solid;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-double"></div>
+        <div>border-style: double;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-double">
+          <div class="example-brick-small"></div>
         </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-none"></div>
-          <div>border-style: none;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-hidden"></div>
-          <div>border-style: hidden;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-dotted"></div>
-          <div>border-style: dotted;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-dashed"></div>
-          <div>border-style: dashed;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-solid"></div>
-          <div>border-style: solid;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-double"></div>
-          <div>border-style: double;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-groove"></div>
-          <div>border-style: groove;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-ridge"></div>
-          <div>border-style: ridge;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-inset"></div>
-          <div>border-style: inset;</div>
-        </div>
-        <div class="display-flex align-items-center">
-          <div class="example-brick border-style-outset"></div>
-          <div>border-style: outset;</div>
-        </div>
+        <div>border-style: double;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-groove"></div>
+        <div>border-style: groove;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-ridge"></div>
+        <div>border-style: ridge;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-inset"></div>
+        <div>border-style: inset;</div>
+      </div>
+      <div class="display-flex align-items-center">
+        <div class="example-brick border-style-outset"></div>
+        <div>border-style: outset;</div>
       </div>
     </div>
   </div>
@@ -75,34 +67,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.buttons > :not(:last-child) {
-  margin-bottom: 0.5rem;
-}
-
-.btn-transparent {
-  display: block;
-  background-color: transparent;
-}
-
-.btn-bordered {
-  border: solid 3px blue;
-}
-
-.btn-only-left-border {
-  border-color: blue;
-  border-width: 0;
-  border-left-width: 6px;
-}
-
-.btn-only-left-border-facet {
-  border-color: transparent;
-  border-left-color: red;
-  border-width: 0;
-  border-width: 6px;
-}
-
-.rounded {
-  border-radius: 6px;
+* {
+  box-sizing: border-box;
 }
 
 .example-group {
@@ -111,10 +77,16 @@ export default {
   }
 
   .example-brick {
-    background-color: lightgrey;
-    width: 5rem;
-    height: 2rem;
+    background-color: lightblue;
+    width: 8rem;
+    height: 4rem;
     margin-right: 1rem;
+    border-width: 0.75rem;
+
+    .example-brick-small {
+      width: 4rem;
+      height: 2rem;
+    }
   }
 }
 
